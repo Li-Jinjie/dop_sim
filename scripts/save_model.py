d26a6a91f829824238afb20709f68087b61ab529
Jinjie Li <lijinjie362@outlook.com>
2023-03-17 20:23:21 +0800

    fix a bug for the python model

--- scripts/save_model.py
+++ scripts/save_model.py
@@ -9,19 +9,23 @@ if __name__ == "__main__":
         mul_qd = mul_qd.to("cuda")
 
     sm_mul_qd = torch.jit.script(mul_qd)
+    # sm_mul_qd = mul_qd  # debug only
 
     """
     ego_states: [player, group, HP, north, east, down, phi, theta, psi, ew, ex, ey, ez, vx, vy, vz, u, v, w,
-             p, q, r, Va, Vg, alpha, beta, gamma, chi, wn, we]   len=30
+             p, q, r, Va, Vg, alpha, beta, gamma, chi, wn, we, wd]   len=31
     cmd: ["roll_rate_cmd", "pitch_rate_cmd", "yaw_rate_cmd", "throttle_cmd"]
     """
-    state = torch.zeros([num_agent, 30, 1], dtype=torch.float64)
+    state = torch.zeros([num_agent, 31, 1], dtype=torch.float64).to("cuda")
     state[:, 9, 0] = 1.0  # ew
 
-    rate_cmd = torch.zeros([num_agent, 4, 1], dtype=torch.float64)
+    rate_cmd = torch.zeros([num_agent, 4, 1], dtype=torch.float64).to("cuda")
 
-    state_new = sm_mul_qd
+    state_new = sm_mul_qd(state, rate_cmd, 0.02)
+
+    print("Model runs successfully!")
 
     # save model
     sm_mul_qd.save("../models/mul_qd_model.pt")
+    print("Model saves successfully!")
 
