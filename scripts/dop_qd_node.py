92fe517795968b782ce9a425129f04c03d855ae6
Jinjie Li <lijinjie362@outlook.com>
2023-03-23 11:55:01 +0800

    add 2 triangles for each qd

--- scripts/dop_qd_node.py
+++ scripts/dop_qd_node.py
@@ -17,30 +17,35 @@ sys.path.append(home + "/ljj_ws/src/dop_qd_sim/scripts")
 
 import rospy
 import torch
-from geometry_msgs.msg import PoseStamped
+from std_msgs.msg import ColorRGBA
+from geometry_msgs.msg import PoseStamped, Point
+from visualization_msgs.msg import MarkerArray, Marker
 from quadrotor import MulQuadrotors
 
 
 class DopQdNode:
     def __init__(self):
         rospy.init_node("dop_qd_node", anonymous=False)
-        # self.num_agent = rospy.get_param("num_agent")
+
+        # params
         self.ts_sim = rospy.get_param(rospy.get_name() + "/ts_sim")  # time step for simulation
         self.ts_ctl = rospy.get_param(rospy.get_name() + "/ts_ctl")  # time step for inner Controller
         self.ts_viz = rospy.get_param(rospy.get_name() + "/ts_viz")  # time step for visualization
 
-        self.qd_init_states = rospy.get_param(rospy.get_name() + "/qd_init_states")
-
-        self.num_agent = len(self.qd_init_states)
+        qd_init_states = rospy.get_param(rospy.get_name() + "/qd_init_states")  # initial states for quadrotors
+        self.num_agent = len(qd_init_states)  # number of quadrotors
+        self.init_ego_states = self._load_init_states(self.num_agent, qd_init_states)
 
         rospy.loginfo(
             f"Load params: \n num_agent: {self.num_agent} \n sim_ts: {self.ts_sim} \n control_ts: {self.ts_ctl}"
         )
 
-        self.pose_pub_list = []
-        for i in range(self.num_agent):
-            self.pose_pub_list.append(rospy.Publisher("qd_" + str(i), PoseStamped, queue_size=1))
+        # visualization
+        self.marker_array_pub = rospy.Publisher("mul_qds_viz", MarkerArray, queue_size=5)
+        self.viz_time = rospy.Time.now()  # the time of the last visualization
+        self._pub_viz(self.init_ego_states, is_first=True)
 
+        # simulation
         self.rate = rospy.Rate(1 / self.ts_sim)
         self.model = self._load_model()  # Load PyTorch model
 
@@ -50,7 +55,7 @@ class DopQdNode:
                  p, q, r, Va, Vg, alpha, beta, gamma, chi, wn, we, wd]   len=31
         cmd: ["roll_rate_cmd", "pitch_rate_cmd", "yaw_rate_cmd", "throttle_cmd"]
         """
-        ego_states = self._load_init_states()
+        ego_states = self.init_ego_states
 
         body_rate_cmd = torch.zeros([self.num_agent, 4, 1], dtype=torch.float64).to("cuda")
         body_rate_cmd[:, 3, 0] = 0.0  # throttle_cmd
@@ -58,26 +63,30 @@ class DopQdNode:
         rospy.loginfo("Start simulation!")
 
         while not rospy.is_shutdown():
-            time_a = time.perf_counter()
+            # time_a = time.perf_counter()
 
             ego_states = self._run_model(ego_states, body_rate_cmd)
-            self._publish_points(ego_states)
 
-            time_b = time.perf_counter()
+            if (rospy.Time.now() - self.viz_time).to_sec() > self.ts_viz:
+                self._pub_viz(ego_states)
+                self.viz_time = rospy.Time.now()
+
+            # time_b = time.perf_counter()
 
             self.rate.sleep()
 
-    def _load_init_states(self):
-        ego_states = torch.zeros([self.num_agent, 31, 1], dtype=torch.float64).to("cuda")
+    @staticmethod
+    def _load_init_states(num_agent: float, qd_init_states: list) -> torch.Tensor:
+        ego_states = torch.zeros([num_agent, 31, 1], dtype=torch.float64).to("cuda")
         ego_states[:, 9, 0] = 1.0  # ew
-        for i in range(self.num_agent):
-            ego_states[i][3][0] = self.qd_init_states[i][0]  # n
-            ego_states[i][4][0] = self.qd_init_states[i][1]  # e
-            ego_states[i][5][0] = self.qd_init_states[i][2]  # d
+        for i in range(num_agent):
+            ego_states[i][3][0] = qd_init_states[i][0]  # n
+            ego_states[i][4][0] = qd_init_states[i][1]  # e
+            ego_states[i][5][0] = qd_init_states[i][2]  # d
 
         return ego_states
 
-    def _load_model(self):
+    def _load_model(self)-> torch.jit.ScriptModule:
         # Load PyTorch model here
         mul_qd = MulQuadrotors(self.num_agent, self.ts_sim, torch.float64).requires_grad_(False)
         if torch.cuda.is_available():
@@ -87,26 +96,57 @@ class DopQdNode:
         rospy.loginfo("Load model successfully!")
         return sm_mul_qd
 
-    def _run_model(self, ego_states: torch.Tensor, body_rate_cmd: torch.Tensor):
+    def _run_model(self, ego_states: torch.Tensor, body_rate_cmd: torch.Tensor)-> torch.Tensor:
         # Run PyTorch model and get output tensor
         output_tensor = self.model(ego_states, body_rate_cmd, self.ts_sim)
         return output_tensor
 
-    def _publish_points(self, ego_states: torch.Tensor):
-        # rospy.loginfo("Publish points for one round!")
+    def _pub_viz(self, ego_states: torch.Tensor, is_first=False)-> None:
+        rospy.loginfo("Publish points for one round!")
+
+        marker_array = MarkerArray()
 
         for i in range(self.num_agent):
-            pose_msg = PoseStamped()
-            pose_msg.header.stamp = rospy.Time.now()
-            pose_msg.header.frame_id = "map"
-            pose_msg.pose.position.x = ego_states[i][4][0]  # e
-            pose_msg.pose.position.y = ego_states[i][3][0]  # n
-            pose_msg.pose.position.z = -ego_states[i][5][0]  # u
-            pose_msg.pose.orientation.w = ego_states[i][9][0]  # ew
-            pose_msg.pose.orientation.x = ego_states[i][10][0]  # ex
-            pose_msg.pose.orientation.y = ego_states[i][11][0]  # ey
-            pose_msg.pose.orientation.z = ego_states[i][12][0]  # ez
-            self.pose_pub_list[i].publish(pose_msg)
+            marker = Marker()
+            marker.header.frame_id = "map"
+            marker.header.stamp = rospy.Time.now()
+            marker.type = marker.TRIANGLE_LIST
+            marker.ns = f"qd_{i}"
+            marker.id = i
+
+            if is_first:
+                marker.action = marker.ADD
+            else:
+                marker.action = marker.MODIFY
+
+            marker.color.a = 1.0  # must be set greater than 0 to be visible. This alpha is for the whole body.
+
+            marker.scale.x = 1.0
+            marker.scale.y = 1.0
+            marker.scale.z = 1.0
+
+            p1 = Point(3, 1, 0)
+            p2 = Point(1, 0, 0)
+            p3 = Point(0, 1, 0)
+
+            p4 = Point(1, 3, 0)
+            p5 = Point(0, 1, 0)
+            p6 = Point(1, 0, 0)
+
+            marker.points = [p1, p2, p3, p4, p5, p6]
+            marker.colors = [ColorRGBA(1.0, 0.5, 0.5, 1.0), ColorRGBA(1.0, 0.5, 0.5, 1.0)]
+
+            marker.pose.position.x = ego_states[i][4][0]  # e
+            marker.pose.position.y = ego_states[i][3][0]  # n
+            marker.pose.position.z = -ego_states[i][5][0]  # u
+            marker.pose.orientation.w = ego_states[i][9][0]  # ew
+            marker.pose.orientation.x = ego_states[i][10][0]  # ex
+            marker.pose.orientation.y = ego_states[i][11][0]  # ey
+            marker.pose.orientation.z = ego_states[i][12][0]  # ez
+
+            marker_array.markers.append(marker)
+
+        self.marker_array_pub.publish(marker_array)
 
 
 if __name__ == "__main__":
